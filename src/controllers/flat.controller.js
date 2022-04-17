0aa695dfa3c4aa72b02d04a5e7aa28cd9f45c993
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-04-17 19:38:19 +0530

    update route added

--- src/controllers/flat.controller.js
+++ src/controllers/flat.controller.js
@@ -6,6 +6,8 @@ const router = express.Router();
 // console.log("jeevan")
 
 
+
+
 router.post("/", async (req, res) => {
     try {
         const flat = await Flat.create(req.body);
@@ -26,7 +28,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     try {
-        console.log(req.params.id)
+        // console.log(req.params.id)
         const flat = await Flat.findById(req.params.id);
         return res.status(201).send(flat)
     } catch (er) {
@@ -35,10 +37,32 @@ router.get("/:id", async (req, res) => {
 })
 
 
+
 router.delete("/:id", async (req, res) => {
     try {
         const flat = await Flat.findByIdAndDelete(req.params.id);
         return res.send(flat)
+    } catch (er) {
+        return res.status(500).send(er.message)
+    }
+})
+
+//findbyidupdate
+router.patch("/:id", async (req, res) => {
+    try {
+        const flat = await Flat.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.send(flat)
+    } catch (er) {
+        return res.status(500).send(er.message)
+    }
+})
+
+//filter by cat
+router.get("/cat/:type", async (req, res) => {
+    try {
+        console.log(req.params.type)
+        const flats = await Flat.find({ type: req.params.type }).lean().exec();
+        return res.status(201).send(flats)
     } catch (er) {
         return res.status(500).send(er.message)
     }
